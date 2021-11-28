abd4229d4611d215e2aa47e5ef7dae1013371837
ED CARLOS DA SILVA PEREIRA <63868640+Goghed@users.noreply.github.com>
2021-11-28 01:36:36 -0300

    update 8

--- src/app/estudante-detalhe/estudante-detalhe.component.ts
+++ src/app/estudante-detalhe/estudante-detalhe.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Estudante } from '../estudante';
+import { EstudanteService } from '../estudante.service';
 
 @Component({
   selector: 'app-estudante-detalhe',
   templateUrl: './estudante-detalhe.component.html',
-  styleUrls: ['./estudante-detalhe.component.css']
+  styleUrls: [ './estudante-detalhe.component.css' ]
 })
 export class EstudanteDetalheComponent implements OnInit {
+  estudante: Estudante | undefined;
 
-  @Input() estudante?: Estudante;
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private estudanteService: EstudanteService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getEstudante();
   }
 
+  getEstudante(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.estudanteService.getEstudante(id)
+      .subscribe(estudante => this.estudante = estudante);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
